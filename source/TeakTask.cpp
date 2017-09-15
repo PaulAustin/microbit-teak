50b13d13df6c01507350046103242fbd0688f6fd
PaulAustin <canoetime@gmail.com>
2017-09-15 16:07:56 -0500

    Animation for entering/exiting active task.

--- source/TeakTask.cpp
+++ source/TeakTask.cpp
@@ -305,38 +305,87 @@ public:
     TopMenuTask();
     TaskId Event(MicroBitEvent event);
     TeakTask* ActiveTask() { return gTasks[m_activeTask]; }
+    TaskId HighlightLine();
+    enum {
+      kTopMenuBrowse,
+      kTopMenuSwipeIn,
+      kTopMenuSwipeOut,
+    };
 public:
     TaskId m_activeTask;
+    int8_t m_highlightFrame;
+    int8_t m_state;
 };
 TopMenuTask gTopMenuTask;
 
 TopMenuTask::TopMenuTask()
 {
     m_activeTask = kFirstInRing;
+    m_state = kTopMenuBrowse;
     m_image = ActiveTask()->PackedImage();
+}
+
+TaskId TopMenuTask::HighlightLine()
+{
+    m_image = ActiveTask()->PackedImage();
+    // Animate bar from top or bottom based on state.
+    int offset = (m_state == kTopMenuSwipeIn)
+        ? m_highlightFrame
+        : (4 - m_highlightFrame);
+    int mask = 0x0000001f << (5 * offset);
+    m_image = m_image | mask;
+
+    // Prep for next frame.
+    if (m_highlightFrame < 5) {
+        // Still animatting,
+        m_highlightFrame++;
+        return kSameTask;
+    } else {
+        // All done, reset the state and frame count.
+        m_highlightFrame = 0;
+        if (m_state == kTopMenuSwipeIn) {
+            // Swipped in the new task, activte it
+            // and leave this teask ready to swipe back in.
+            m_state = kTopMenuSwipeOut;
+            return m_activeTask;
+        } else {
+            // The old task has been swipped out , change the state
+            // and stay with the menu task.
+            m_state = kTopMenuBrowse;
+            return kSameTask;
+        }
+    }
 }
 
 TaskId TopMenuTask::Event(MicroBitEvent event)
 {
-    if (event.value == MICROBIT_BUTTON_EVT_CLICK) {
-        if (event.source == MICROBIT_ID_BUTTON_A) {
-            m_activeTask = gScrollTask.Setup(m_activeTask, true);
-            return kScrollTask;
-        } else if (event.source == MICROBIT_ID_BUTTON_B) {
-            m_activeTask = gScrollTask.Setup(m_activeTask, false);
-            return kScrollTask;
+    switch(m_state) {
+    case kTopMenuSwipeIn:
+    case kTopMenuSwipeOut:
+        if (event.source == MICROBIT_ID_TIMER) {
+          return HighlightLine();
         }
-    } else if (event.value == MICROBIT_BUTTON_EVT_HOLD) {
-        if (event.source == MICROBIT_ID_BUTTON_AB) {
-            // activeate the advanced mode for the task.
-            return m_activeTask;
-            // uBit.display.print('H');
+        break;
+    case kTopMenuBrowse:
+        if (event.value == MICROBIT_BUTTON_EVT_CLICK) {
+            m_state = kTopMenuBrowse;
+            if (event.source == MICROBIT_ID_BUTTON_A) {
+                m_activeTask = gScrollTask.Setup(m_activeTask, true);
+                return kScrollTask;
+            } else if (event.source == MICROBIT_ID_BUTTON_B) {
+                m_activeTask = gScrollTask.Setup(m_activeTask, false);
+                return kScrollTask;
+            }
+        } else if (event.value == MICROBIT_BUTTON_EVT_HOLD) {
+            if (event.source == MICROBIT_ID_BUTTON_AB) {
+                m_state = kTopMenuSwipeIn;
+                return kSameTask;
+            }
+        } else if (event.source == MICROBIT_ID_TIMER) {
+            m_image = ActiveTask()->PackedImage();
         }
-    } else if (event.source == MICROBIT_ID_TIMER) {
-      //  ActiveTask()->Event(event);
-        m_image = ActiveTask()->PackedImage();
+        break;
     }
-    // else if timer then send it to highlighte task
     return kSameTask;
 }
 
@@ -436,7 +485,7 @@ TaskId BlueToothTask::Event(MicroBitEvent event)
       return kTopMenuTask;
   } else {
       return kSameTask;
-  } 
+  }
 }
 //------------------------------------------------------------------------------
 // A teask to use the built-in level (accelerometer)
